932199dca72703e16dfe3ac82ab6e615f489453b
Lschaaf24 <Liam2.Schaaf@live.uwe.ac.uk>
2026-02-01 15:21:19 +0000

    End Game

---
+++ Mask/Assets/Scripts/General/EndGame.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject EndGameScreen;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            EndGameScreen.SetActive(true);
+        }
+    }
+}
